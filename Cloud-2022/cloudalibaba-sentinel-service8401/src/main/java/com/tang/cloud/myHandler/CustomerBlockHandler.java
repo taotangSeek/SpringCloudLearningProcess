a34de95c3311c7b5f8847afbca6237826c6c2118
taotangSeek <tjutt19@163.com>
2022-04-07 11:44:02 +0800

    sentinel

---
+++ Cloud-2022/cloudalibaba-sentinel-service8401/src/main/java/com/tang/cloud/myHandler/CustomerBlockHandler.java
@@ -0,0 +1,16 @@
+package com.tang.cloud.myHandler;
+
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.tang.cloud.entities.CommonResult;
+
+
+public class CustomerBlockHandler {
+    public static CommonResult handlerException(BlockException exception) {
+        return new CommonResult(4444,"按客戶自定义,global handlerException----1");
+    }
+
+    public static CommonResult handlerException2(BlockException exception) {
+        return new CommonResult(4444,"按客戶自定义,global handlerException----2");
+    }
+}
+
